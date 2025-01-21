cd74800f4b84128cbd3208a96d75b96e54c09ee8
Minimert989 <ryuminwoo47298@gmail.com>
2025-01-22 02:06:29 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,1 @@
-const serverless = require("serverless-http");
-const express = require("express");
 
-const app = express();
-
-// Middleware
-app.use(express.json());
-
-// Example Routes
-app.get("/", (req, res) => {
-    res.send("Hello from Netlify Functions!");
-});
-
-app.get("/api/data", (req, res) => {
-    res.json({ message: "This is your backend data!" });
-});
-
-// Export as a Netlify function
-module.exports.handler = serverless(app);
